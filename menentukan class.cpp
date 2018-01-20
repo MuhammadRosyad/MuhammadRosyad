2e87c1cb835c7aeeed760f131725255c00dc7209
MuhammadRosyad <achaibuy01@gmail.com>
2018-01-20 13:55:25 +0800

    memasukkan class

--- menentukan class.cpp
+++ menentukan class.cpp
@@ -6,4 +6,11 @@ class Negara{
 		string kekuatan, ekonomi, pertahanan;
 };
 
+int main (){
+	Negara Amerika, China, Rusia;
+	
+	Amerika.kekuatan = "Teknologi Modern";
+	China.kekuatan = "Kemampuan Manusianya";
+	Rusia.kekuatan = "Teknologi Nuklir";
+
 
